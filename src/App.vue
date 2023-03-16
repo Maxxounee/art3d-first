7f55ac45565c7712b02a97b6c1ca1f58f4ad9430
maxxounee <macbook@MacBook-Pro-MacBook-2.local>
2023-03-16 23:57:28 +0300

    правки

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import PieChart from "@/components/PieChart.vue";
 
 export default {
   name: 'App',
+
   components: {
     PieChart
   }
@@ -20,6 +21,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,56 +1,55 @@
 <template>
 <div class="wrapper">
   <div class="chart">
-    <div @mouseover.stop="darkenColor"
-         @mouseleave="resetColor"
-         class="chart-green"
-         id="green"
+    <div
+        @mouseover.stop="darkenColor('green')"
+        @mouseleave="resetColor"
+        class="chart-green"
+        :class="{_chartActive: activeSegment === 'green'}"
     >
     </div>
-
     <div
-        @mouseover="darkenColor"
+        @mouseover="darkenColor('pink')"
         @mouseleave="resetColor"
         class="chart-pink"
-        id="pink"
+        :class="{_chartActive: activeSegment === 'pink'}"
     >
     </div>
-
     <div
-        @mouseover="darkenColor"
+        @mouseover="darkenColor('grey')"
         @mouseleave="resetColor"
         class="chart-grey"
-        id="grey"
+        :class="{_chartActive: activeSegment === 'grey'}"
     >
     </div>
   </div>
-
   <div class="legend">
-    <div class="legend__text-wrapper">
-      <div class="legend__pink_color"></div>
-      <p class="legend__pink"
-         :class="{_activePink: activeSegment === 'pink'}"
-      >РОЗОВЫЙ СЕГМЕНТ</p>
+    <div
+        class="legend__text-wrapper"
+        @mouseover="darkenColor('pink')"
+        @mouseleave="resetColor"
+    >
+      <div class="legend__pink_color" :class="{_activeSquare: activeSegment === 'pink'}"></div>
+      <p class="legend__pink" :class="{_activeLegendPink: activeSegment === 'pink'}">РОЗОВЫЙ СЕГМЕНТ</p>
     </div>
-
-    <div class="legend__text-wrapper">
-      <div class="legend__green_color"></div>
-      <p class="legend__green"
-         :class="{_activeGreen: activeSegment === 'green'}"
-      > ЗЕЛЕНЫЙ СЕГМЕНТ</p>
+    <div
+        class="legend__text-wrapper"
+        @mouseover="darkenColor('green')"
+        @mouseleave="resetColor"
+    >
+      <div class="legend__green_color" :class="{_activeSquare: activeSegment === 'green'}"></div>
+      <p class="legend__green" :class="{_activeLegendGreen: activeSegment === 'green'}">ЗЕЛЕНЫЙ СЕГМЕНТ</p>
     </div>
-
-
-
-    <div class="legend__text-wrapper">
-      <div class="legend__grey_color"></div>
-      <p class="legend__grey"
-         :class="{_activeGrey: activeSegment === 'grey'}"
-      >СЕРЫЙ СЕГМЕНТ</p>
+    <div
+        class="legend__text-wrapper"
+        @mouseover="darkenColor('grey')"
+        @mouseleave="resetColor"
+    >
+      <div class="legend__grey_color" :class="{_activeSquare: activeSegment === 'grey'}"></div>
+      <p class="legend__grey" :class="{_activeLegendGrey: activeSegment === 'grey'}">СЕРЫЙ СЕГМЕНТ</p>
     </div>
   </div>
 </div>
-
 </template>
 
 <script>
@@ -61,15 +60,12 @@ export default {
     }
   },
   methods: {
-  darkenColor(event) {
-    event.target.style.filter = "brightness(80%)"
-    this.activeSegment = event.target.id
-    console.log(event.target.id)
-  },
-  resetColor(event) {
-    this.activeSegment = ''
-    event.target.style.filter = "none"
-  }
+    darkenColor(color) {
+      this.activeSegment = color
+      },
+    resetColor() {
+      this.activeSegment = ''
+    }
   }
 }
 </script>
@@ -79,7 +75,6 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  color: #D9D9D9;
 }
 
 .wrapper {
@@ -92,6 +87,7 @@ export default {
 }
 
 .chart{
+  z-index: 100;
   width: 516px;
   height: 516px;
   position: relative;
@@ -106,46 +102,63 @@ export default {
   top: 0;
   width: 100%;
   height: 100%;
+  filter: brightness(100%);
+  transition-property: filter;
+  transition-duration: 0.6s;
 }
 
 .chart-green{
   background-color: #97BB31;
-  clip-path: polygon(48.75% 50%, 0% 17%, 0% 100%, 60% 100%);
+  clip-path: polygon(48.75% 50%, 0% -25%, 0% 100%, 60% 100%);
 }
 
 .chart-pink{
   background-color: #FF69B4;
-  clip-path: polygon(48.75% 50%, 0% 17%, 0% 0%, 100% 0%, 100% 50%);
+  clip-path: polygon(48.75% 50%, 0% -25%, 0% 0%, 100% 0%, 100% 50%);
 }
 
 .chart-grey{
   background-color: #D9D9D9;
-  position: absolute;
   clip-path: polygon(48.75% 50%, 42% 100%, 100% 100%, 100% 50%);
 }
 
-._activeGreen,
-._activePink,
-._activeGrey {
-  color: black;
-  text-decoration: underline;
+._chartActive {
+  filter: brightness(75%);
 }
 
-._activeGreen {
+.legend__pink,
+.legend__green,
+.legend__grey {
+  color: #D9D9D9;
+  text-decoration: underline;
+  text-underline-offset: 3px;
+  text-decoration-color: transparent;
+  user-select: none;
+  transition: 0.7s;
+}
+
+._activeLegendGreen,
+._activeLegendPink,
+._activeLegendGrey {
+  color: black;
+}
+
+._activeLegendGreen {
   text-decoration-color: #97BB31;
 }
 
-._activePink {
+._activeLegendPink {
   text-decoration-color: #FF69B4;
 }
 
-._activeGrey {
+._activeLegendGrey {
   text-decoration-color: #D9D9D9;
 }
 
 .legend__text-wrapper {
   display: flex;
   align-items: center;
+  margin-bottom: 8px;
 }
 
 .legend__green_color,
@@ -154,11 +167,12 @@ export default {
   margin-right: 10px;
   width: 10px;
   height: 10px;
+  transform: rotate(0deg);
+  transition: 0.6s;
 }
 
 .legend__green_color {
   background: #97BB31;
-
 }
 
 .legend__pink_color {
@@ -167,5 +181,9 @@ export default {
 
 .legend__grey_color {
   background: #D9D9D9;
+}
+
+._activeSquare {
+  transform: rotate(180deg);
 }
 </style>
